0ba37f9c551079cb1f13e34194afd01ac183b36c
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-03-19 17:48:07 +0100

    feat: :sparkles: implement methods for each parts of the processus

--- processus.sh
+++ processus.sh
@@ -1,53 +1,94 @@
 #!/bin/bash
 
-while true; do
-    clear
-    echo "Menu:"
-    echo "1. Identifier tous les processus présents sur le système et indiquer leur propriétaire"
-    echo "2. Identifier uniquement les processus actifs et indiquer leur propriétaire"
-    echo "3. Identifier les processus appartenant à un utilisateur donné en paramètre"
-    echo "4. Identifier les processus consommant le plus de mémoire et indiquer leur propriétaire"
-    echo "5. Identifier les processus dont le nom contient une chaine de caractères (définie en paramètre) et indiquer leur propriétaire"
-    echo "6. Produire un fichier de sortie contenant le résultat de la recherche effectuée qui n’écrase pas les résultats de la recherche précédente (tenir compte par exemple de la date et l’heure système)"
-    echo "7. Proposer une fonctionnalité supplémentaire que vous jugez intéressante et qui manque à la liste précédente (argumentez votre choix)"
-    echo "8. Quitter"
+# Fonction pour afficher la liste de tous les processus
+function list_all_processes() {
+  echo "Liste de tous les processus:"
+  ps -ef
+}
+
+# Fonction pour afficher la liste des processus actifs
+function list_active_processes() {
+  echo "Liste des processus actifs:"
+  ps -ef | grep -v defunct | grep -v "<defunct>"
+}
+
+# Fonction pour afficher la liste des processus d'un utilisateur donné
+function list_user_processes() {
+  user=$1
+  echo "Liste des processus de l'utilisateur $user:"
+  ps -u "$user"
+}
+
+# Fonction pour afficher la liste des processus consommant le plus de mémoire
+function list_memory_processes() {
+  echo "Liste des processus consommant le plus de mémoire:"
+  ps -eo pid,user,%mem,command --sort=-%mem | head
+}
+
+# Fonction pour afficher la liste des processus dont le nom contient une chaîne de caractères
+function list_matching_processes() {
+  pattern=$1
+  echo "Liste des processus dont le nom contient '$pattern':"
+  ps -eo pid,user,%mem,command | grep "$pattern"
+}
+
+# Fonction pour écrire la liste des processus correspondant à un critère de recherche dans un fichier
+function write_processes_to_file() {
+  pattern=$1
+  file="processes_$(date +'%Y-%m-%d_%H-%M-%S').txt"
+  echo "Écriture de la liste des processus correspondant à '$pattern' dans le fichier '$file'..."
+  ps -eo pid,user,%mem,command | grep "$pattern" > "$file"
+}
+
+# Fonction pour afficher le menu principal
+function show_main_menu() {
+  while true; do
+    echo "Menu principal:"
+    echo "  1) Liste de tous les processus"
+    echo "  2) Liste des processus actifs"
+    echo "  3) Liste des processus d'un utilisateur donné"
+    echo "  4) Liste des processus consommant le plus de mémoire"
+    echo "  5) Liste des processus dont le nom contient une chaîne de caractères"
+    echo "  6) Écrire la liste des processus correspondant à un critère de recherche dans un fichier"
+    echo "  7) Fonctionnalité supplémentaire"
+    echo "  8) Quitter"
     read -p "Entrez votre choix: " choice
 
     case $choice in
-        1)
-            ps aux
-            ;;
-        2)
-            ps r | 
-            ;;
-        3)
-            read -p "Entrez le nom d'utilisateur: " username
-            pgrep -u $username 
-            ;;
-        4)
-            ps aux --sort=-%mem | head
-            ;;
-        5)
-            read -p "Entrez la chaîne de caractères: " string
-            pgrep -f $string
-            ;;
-        6)
-             output_file="processes_$(date +'%Y-%m-%d_%H-%M-%S').txt"
-             ps aux |  > $output_file
-             echo "Résultats enregistrés dans $output_file"
-             ;;
-        7)
-             ps aux --sort=-%cpu | head
-             ;; 
-        8)
-             exit 0
-             ;;
-        *)
-           echo "Choix invalide."
-           ;;
-   esac
+      1)
+        list_all_processes
+        ;;
+      2)
+        list_active_processes
+        ;;
+      3)
+        read -p "Entrez le nom d'utilisateur: " user
+        list_user_processes "$user"
+        ;;
+      4)
+        list_memory_processes
+        ;;
+      5)
+        read -p "Entrez la chaîne de caractères: " pattern
+        list_matching_processes "$pattern"
+        ;;
+      6)
+        read -p "Entrez la chaîne de caractères: " pattern
+        write_processes_to_file "$pattern"
+        ;;
+      7)
+        echo "Fonctionnalité supplémentaire à ajouter..."
+        ;;
+      8)
+        echo "Au revoir!"
+        exit 0
+        ;;
+      *)
+        echo "Choix invalide"
+        ;;
+    esac
 
-   read -p "Entrez une chaîne de caractères pour filtrer les résultats (laissez vide pour ne pas filtrer): " search_term
-
-   sleep 30s
-done
+    # L’affichage du résultat sera rafraichi automatiquement toutes les 30 secondes.
+    sleep 30
+    done
+}
